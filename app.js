f15a5abc2288094cf97034d29aa9ba0fd87d1e95
Herzuull <herzuull@gmail.com>
2016-07-01 17:09:41 +0200

    Ajout du système de templates

--- app.js
+++ app.js
@@ -1,15 +1,29 @@
 var express = require('express');
 var app = express();
+var expressLayouts = require('express-ejs-layouts');
 
 app.set('port', (process.env.PORT || 1999));
 
 app.use(express.static(__dirname + '/www/'));
-
 // views is directory for all template files
-app.set('views', __dirname + '/www/');
+app.set('view engine', 'ejs');
 
-app.get('/', function(request, response) {
-  response.render('index');
+app.use(expressLayouts);
+
+app.get('/', function(req, res) {
+  res.render('pages/index', {page: 'index'});
+});
+
+app.get('/drawings', function(req, res) {
+  res.render('pages/drawings', {page: 'drawings'});
+});
+
+app.get('/games', function(req, res) {
+  res.render('pages/games', {page: 'games'});
+});
+
+app.get('/cv', function(req, res) {
+  res.render('pages/cv', {page: 'cv'});
 });
 
 app.listen(app.get('port'), function() {
--- sources/js/index.js
+++ sources/js/index.js
@@ -1,2 +1,23 @@
 window.addEventListener('load', function() {
+
+  var viewContent = document.querySelector('#contentObject');
+  viewContent.setAttribute('data', './news.html');
+  var content = (viewContent.contentWindow || viewContent.contentDocument);
+  var contentDocument = content.document || content;
+  document.querySelector('#content').innerHTML = contentDocument.body.innerHTML;
+
+  var navButtons = document.querySelectorAll('.nav');
+  Array.from(navButtons).forEach(function(item) {
+    item.addEventListener('click', function(e) {
+      e.preventDefault();
+      e.stopPropagation();
+      var url = e.target.getAttribute('href');
+      var viewContent = document.querySelector('#contentObject');
+      viewContent.setAttribute('data', url);
+      var content = (viewContent.contentWindow || viewContent.contentDocument);
+      var contentDocument = content.document || content;
+      document.querySelector('#content').innerHTML = contentDocument.body.innerHTML;
+      return false;
+    }, false);
+  });
 }, false);
