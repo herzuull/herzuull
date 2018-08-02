2c84841d2bca6e823610bc3f0a5ef138ec1c8355
herzuull <herzuull@gmail.com>
2018-08-02 15:06:51 +0200

    Added log to chec session in heroku

--- app/controller/Locale.js
+++ app/controller/Locale.js
@@ -1,10 +1,12 @@
 module.exports = {
   update: function(req, res, next) {
     const locales = process.env.LOCALES.split(',')
+    console.log('Session > ', req.session)
     req.session.locale =
       req.params.locale && locales.indexOf(req.params.locale) !== -1
         ? req.params.locale
         : res.session.locale
+    console.log('Session > ', req.session)
     res.redirect('back')
   },
   set: function(req, res, next) {
